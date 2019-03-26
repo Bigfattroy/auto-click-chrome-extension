b60fc64956d16ef576c2948a6c98c9e7cd48873b
Huynh Duc Khoan <huynhduckhoan@gmail.com>
2019-03-26 11:08:40 +0700

    Add bentai site.. :V :V

--- background.js
+++ background.js
@@ -3,5 +3,7 @@ chrome.tabs.create
 chrome.browserAction.onClicked.addListener(function (tab) {
     var newURL = "https://truyen.pro/wp-admin/admin.php?page=dragon_leech";
     chrome.tabs.create({ url: newURL });
+    var newURL = "https://hentaihay.top/wp-admin/admin.php?page=dragon_leech";
+    chrome.tabs.create({ url: newURL });
     chrome.tabs.executeScript({ file: "content.js" });
 });
--- content.js
+++ content.js
@@ -1,5 +1,23 @@
-function clickAutoClick() {
-    document.querySelector('.btn-auto-update').click();
+function clickTruyen() {
+    document.querySelector('.btn-auto-update-truyen').click();
 };
-clickAutoClick();
-setInterval(clickAutoClick, 24 * 60 * 60 * 1000);
+function clickHentai() {
+    document.querySelector('.btn-auto-all-novel-hentai').click();
+    setTimeout(function(){ 
+        document.querySelector('.btn-auto-update-hentai').click();
+    }, 20 * 60 * 1000);
+
+};
+
+clickTruyen();
+
+setTimeout(function(){ 
+    clickHentai();
+}, 60 * 60 * 1000);
+
+
+setInterval(clickTruyen, 24 * 60 * 60 * 1000);
+
+setTimeout(function(){ 
+    setInterval(clickHentai, 24 * 60 * 60 * 1000);
+}, 30 * 60 * 1000);
