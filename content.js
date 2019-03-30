f21ef4c98b7a422e4bcb7573ff0171a58767d627
Huynh Duc Khoan <huynhduckhoan@gmail.com>
2019-03-30 15:32:11 +0700

    Fix some bugs

--- content.js
+++ content.js
@@ -2,22 +2,19 @@ function clickTruyen() {
     document.querySelector('.btn-auto-update-truyen').click();
 };
 function clickHentai() {
-    document.querySelector('.btn-auto-all-novel-hentai').click();
-    setTimeout(function(){ 
+    document.querySelector('.btn-auto-update-hentai').click();
+    setTimeout(function () {
         document.querySelector('.btn-auto-update-hentai').click();
     }, 20 * 60 * 1000);
 
 };
 
-clickTruyen();
-
-setTimeout(function(){ 
-    clickHentai();
-}, 60 * 60 * 1000);
-
-
-setInterval(clickTruyen, 24 * 60 * 60 * 1000);
-
-setTimeout(function(){ 
-    setInterval(clickHentai, 24 * 60 * 60 * 1000);
-}, 30 * 60 * 1000);
+if (window.location.href == "https://hentaihay.top/wp-admin/admin.php?page=dragon_leech") {
+    setTimeout(function () {
+        clickHentai();
+        setInterval(clickHentai, 24 * 60 * 60 * 1000);
+    }, 30 * 60 * 1000);
+} else {
+    clickTruyen();
+    setInterval(clickTruyen, 24 * 60 * 60 * 1000);
+}
